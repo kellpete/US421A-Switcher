3c4a442b01a5b7fb53f07726c15822d1dd9e272e
Peter Saladin <peter.keller@mht.ch>
2021-02-20 10:38:13 +0100

    posix-style options. Removed unused code

--- US421A-Switcher/Us421aSwitcher.cpp
+++ US421A-Switcher/Us421aSwitcher.cpp
@@ -7,113 +7,6 @@
 
 using namespace std;
 
-int RunTest() {
-
-  auto list = PeripherialSwitchUs421a::GetDeviceList();
-
-  if (list.empty()) {
-    // No device found
-    return 1;
-  }
-  {
-    PeripherialSwitchUs421a usb_switch(list.at(0));
-    usb_switch.Select();
-  }
-
-  this_thread::sleep_for(5s);
-  PeripherialSwitchUs421a usb_switch(PeripherialSwitchUs421a::GetDeviceList().at(0));
-  auto stat = usb_switch.ReadStatus();
-  cout << stat.ToString() << endl;
-
-  usb_switch.Lock(); cout << "Lock" << endl;
-
-  for (size_t i = 0; i < 5; i++)
-  {
-    auto stat = usb_switch.ReadStatus();
-    cout << stat.ToString() << endl;
-
-    usb_switch.LockKeepAlive();
-    this_thread::sleep_for(1s);
-  }
-
-
-  usb_switch.Unlock();
-  cout << "Continuously unlocking" << endl;
-  uint8_t cmd[] = { 0x02, 0x41 };
-
-  for (size_t i = 0; i < 5; i++)
-  {
-    auto stat = usb_switch.ReadStatus();
-    cout << stat.ToString() << endl;
-    usb_switch.SendCommand(cmd);
-    this_thread::sleep_for(1s);
-  }
-
-
-  return 0;
-
-}
-
-void JustObserve() {
-  cout << "Just observing..." << endl;
-  for (size_t i = 0; i < 5 * 60; i++)
-  {
-    auto list = PeripherialSwitchUs421a::GetDeviceList();
-
-    if (!list.empty()) {
-      try {
-        PeripherialSwitchUs421a usb_switch(list.at(0));
-        auto stat = usb_switch.ReadStatus();
-        cout << stat.ToString() << endl;
-      }
-      catch (const std::exception& ex) {
-        cout << "Exception: " << ex.what() << endl;
-      }
-    }
-    else {
-      cout << "No device found" << endl;
-    }
-
-    this_thread::sleep_for(1s);
-  }
-  cout << "Finished" << endl;
-}
-
-void JustObserveAvoidEnumeration() {
-  cout << "Just observing..." << endl;
-  std::unique_ptr<PeripherialSwitchUs421a> dev;
-  auto list = PeripherialSwitchUs421a::GetDeviceList();
-  if (!list.empty()) {
-    dev = make_unique<PeripherialSwitchUs421a>(list.at(0));
-  }
-  for (size_t i = 0; i < 5 * 60; i++)
-  {
-    if (!dev) {
-      list = PeripherialSwitchUs421a::GetDeviceList();
-      if (!list.empty()) {
-        dev = make_unique<PeripherialSwitchUs421a>(list.at(0));
-      }
-    }
-
-    if (dev) {
-      try {
-        auto stat = dev->ReadStatus();
-        cout << stat.ToString() << endl;
-      }
-      catch (const std::exception& ex) {
-        cout << "Exception: " << ex.what() << endl;
-        dev.reset();
-      }
-    }
-    else {
-      cout << "No device found" << endl;
-    }
-
-    this_thread::sleep_for(1s);
-  }
-  cout << "Finished" << endl;
-}
-
 PeripherialSwitchUs421a GetUsbSwitch()
 {
   auto list = PeripherialSwitchUs421a::GetDeviceList();
@@ -188,22 +81,22 @@ int main(int argc, const char* argv[])
     if (argc == 1)
     {
       SwitchToThisPC();
-    } else if(argc == 2 && string{argv[1]} == "/lock")
+    } else if(argc == 2 && string{argv[1]} == "--lock")
     {
         SwitchAndKeepLocked();
     }
-    else if (argc == 2 && string{ argv[1] } == "/cancel")
+    else if (argc == 2 && string{ argv[1] } == "--cancel")
     {
       auto usb_switch = GetUsbSwitch();
       usb_switch.CancelSwitchRequest();
     }
-    else if (argc == 2 && string{ argv[1] } == "/status")
+    else if (argc == 2 && string{ argv[1] } == "--status")
     {
       auto usb_switch = GetUsbSwitch();
       auto status = usb_switch.ReadStatus();
       cout << status.ToString() << endl;
     }
-    else if (argc == 2 && string{ argv[1] } == "/statusloop")
+    else if (argc == 2 && string{ argv[1] } == "--statusloop")
     {
       while (true) {
         try {
@@ -222,16 +115,12 @@ int main(int argc, const char* argv[])
     {
       cout << "Usage:" << endl;
       cout << "If executed without arguments, will try to switch to this host" << endl;
-      cout << "/lock       switches to this PC and keeps a lock until the utility is terminated" << endl;
-      cout << "/cancel     cancels a switch request" << endl;
-      cout << "/status     shows device status" << endl;
-      cout << "/statusloop shows device status continuously" << endl;
+      cout << "--lock       switches to this host and keeps a lock until the utility is terminated" << endl;
+      cout << "--cancel     cancels a switch request" << endl;
+      cout << "--status     shows device status" << endl;
+      cout << "--statusloop shows device status continuously" << endl;
       cout << "" << endl;
     }
-
-    //return RunTest();
-    //JustObserve();
-    //JustObserveAvoidEnumeration();
 
   }
   catch (const std::exception& ex) {
